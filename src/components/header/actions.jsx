3254fd59b1f119c20d53385b855c0ff0f4bec4d2
Kamil Słabosz <slaboszkamil@gmail.com>
2022-09-11 12:04:27 +0200

    add cart button on main page

--- src/components/header/actions.jsx
+++ src/components/header/actions.jsx
@@ -1,11 +1,13 @@
 import { PureComponent } from 'react';
 import cart from '../../images/svg/cart.svg'
+import vector from '../../images/svg/vector.svg'
 
 
 class Actions extends PureComponent {
     render(){
     return <div className='actions'>
-            <p className='action-item'>$ v</p>
+            <p className='action-item'>$ <img
+            src={vector} alt='arrow-down'/></p>
             <img src={cart} id='cart' alt='cart' className='action-item'/>
         </div>
     }
--- src/components/main_page/product-card.jsx
+++ src/components/main_page/product-card.jsx
@@ -1,8 +1,9 @@
 import { PureComponent } from 'react';
 import cart from "../../images/svg/cart.svg"
+import emptyCart from "../../images/svg/emptyCart.svg"
+
 
 class ProductCard extends PureComponent{
-    
 
     render(){
         return <div className='product-card'>
@@ -12,6 +13,14 @@ class ProductCard extends PureComponent{
             alt={this.props.product.name}
             className={this.props.product.inStock ? "product-img" : "product-img img-grayed"}/>
             {this.props.product.inStock ? null : <h1 className='out-of-stock'>OUT OF STOCK</h1>}
+            </div>
+            <div 
+            className={this.props.product.inStock ? 'cart-btn green-bg' : 'cart-btn grayed-bg'}
+            >
+                <img src={emptyCart}
+                alt='add-to-cart' 
+                className='add-to-cart'
+                />
             </div>
             <h1 className={this.props.product.inStock ? "product-name" : "product-name grayed"}>
                 {this.props.product.name}
